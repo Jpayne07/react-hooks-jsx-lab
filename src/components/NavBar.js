317a9fdbf32310c922fa5b3e532ad638510be7b1
Jacob <jacobpayne007@gmail.com>
2024-07-05 20:27:02 -0500

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  NavBar = <nav>
+    <a href = '#home'>Home</a>
+    <a href = '#about'>About</a>
+  </nav>
+  return NavBar
 }
 
 export default NavBar;
